5b9b9690795402e4329a8709147cedaa1ed6cbb5
bintoYu <bintoyu@126.com>
2018-11-11 23:33:18 +0800

    商品展示controller还没写完

--- src/main/java/com/binto/sell/SellApplication.java
+++ src/main/java/com/binto/sell/SellApplication.java
@@ -10,3 +10,4 @@ public class SellApplication {
         SpringApplication.run(SellApplication.class, args);
     }
 }
+ 
--- src/main/java/com/binto/sell/controller/BuyerProductController.java
+++ src/main/java/com/binto/sell/controller/BuyerProductController.java
@@ -1,18 +1,43 @@
 package com.binto.sell.controller;
 
+import com.binto.sell.VO.ProductInfoVO;
+import com.binto.sell.VO.ProductVO;
 import com.binto.sell.VO.ResultVO;
+import com.binto.sell.dataobject.ProductInfo;
+import com.binto.sell.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/list")
     public ResultVO list()
     {
+        //1. 查询所有上架商品
+        List<ProductInfo> productInfoList = productService.findUpAll();
+
+        //2. 查询类目
+
+        //3. 数据拼装
         ResultVO resultVO = new ResultVO();
+        resultVO.setCode(0);
+        resultVO.setMsg("成功");
+
+        ProductVO productVO = new ProductVO();
+        ProductInfoVO productInfoVO = new ProductInfoVO();
+        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
+        resultVO.setData(productVO);
+
         return resultVO;
     }
 }
